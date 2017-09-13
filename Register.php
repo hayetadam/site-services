d60f5d2dff84ececd4535c917f6ed9b0ec509a22
hayet <habethayet@gmail.com>
2017-09-13 10:11:32 +0200

    database

--- Register.php
+++ Register.php
@@ -3,13 +3,16 @@
 include_once './User.php';
 include_once './DataBase.php';
 
-if (!empty($_POST['pseudo'])&&!empty($_POST['mdp'])) {
+/*if (!empty($_POST['pseudo'])&&!empty($_POST['mdp'])) {
     
     $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
     $user = new User($post['pseudo'], md5($post['mdp']), $post['avatar'], $post['genre'], $post['age']);
     //creer une instance pour DataBase qui permet de creer l'utilisateur
+    
     $data = new DataBase();
     $data->createUser($user);
+    
+    
      header("location:index.php");
 
     session_start();
@@ -18,5 +21,18 @@ if (!empty($_POST['pseudo'])&&!empty($_POST['mdp'])) {
     //$_SESSION['genre'] = $post['genre'];
    //$_SESSION['age'] = $post['age'];
 }
+*/
+
+     if (!empty($_POST['pseudo'])&&!empty($_POST['mdp'])){
+    $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    $user = new DataBase();
+    $user->createUser(new User($post['pseudo'], md5($post['mdp']), $post['avatar'], $post['genre'], $post['age']));
+    header("location:index.php");
+
+  session_start();
+    $_SESSION['nom'] = $post['pseudo'];
+}
+
+
 ?>
    
